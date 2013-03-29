d9b06ab360ba57745854165718fcb5e1be809c7b
Brad McAlister <github@sonicparke.com>
2013-03-29 12:59:17 -0500

    trying html5 mode

--- js/src/build/app.built.annotate.js
+++ js/src/build/app.built.annotate.js
@@ -1,5 +1,11 @@
 var app = angular.module('Guzzio', ['ui.bootstrap']);
 app.config([
+  '$locationProvider',
+  function ($locationProvider) {
+    $locationProvider.html5Mode(true);
+  }
+]);
+app.config([
   '$routeProvider',
   function ($routeProvider) {
     $routeProvider.when('/AddFuelUp', {
